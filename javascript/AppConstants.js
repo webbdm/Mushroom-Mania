441d5c7e144cbf75d4a80487c13aaddf8cb8dac2
webbdm <gwebb762@gmail.com>
2017-05-13 17:27:33 -0500

    Firebase data is correctly logging

--- javascript/AppConstants.js
+++ javascript/AppConstants.js
@@ -1,8 +1,9 @@
 app.constant("FIREBASE_CONFIG", {
-    apiKey: "AIzaSyDSFOK40TO6JZXbn_uWpLbuYt1eAjzYcHQ",
-    authDomain: "ng-todo-7d8d1.firebaseapp.com",
-    databaseURL: "https://ng-todo-7d8d1.firebaseio.com",
-    projectId: "ng-todo-7d8d1",
-    storageBucket: "ng-todo-7d8d1.appspot.com",
-    messagingSenderId: "61131669006"
+    apiKey: "AIzaSyDp8Tp586vLQsJTjJ7mb4eFMKpeI_m1XFQ",
+    authDomain: "mushroom-mania-a06d3.firebaseapp.com",
+    databaseURL: "https://mushroom-mania-a06d3.firebaseio.com",
+    projectId: "mushroom-mania-a06d3",
+    storageBucket: "mushroom-mania-a06d3.appspot.com",
+    messagingSenderId: "951584903243"
 });
+
