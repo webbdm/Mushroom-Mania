78613b603a778daff68b849b0f9c9207343a631c
webbdm <gwebb762@gmail.com>
2017-05-13 21:57:22 -0500

    Update AppConstants.js

--- javascript/AppConstants.js
+++ javascript/AppConstants.js
@@ -1,9 +1,4 @@
 app.constant("FIREBASE_CONFIG", {
-    apiKey: "AIzaSyDp8Tp586vLQsJTjJ7mb4eFMKpeI_m1XFQ",
-    authDomain: "mushroom-mania-a06d3.firebaseapp.com",
-    databaseURL: "https://mushroom-mania-a06d3.firebaseio.com",
-    projectId: "mushroom-mania-a06d3",
-    storageBucket: "mushroom-mania-a06d3.appspot.com",
-    messagingSenderId: "951584903243"
+    
 });
 
